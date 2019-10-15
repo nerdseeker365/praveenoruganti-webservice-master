93a2f01d88537eb49e1bfb3013fa88325e6cc1eb
praveen <praveen@praveen-PC>
2019-10-16 00:50:56 +0530

    updated

---
+++ praveenoruganti-hms-restfulws-master/src/main/java/com/praveen/hms/controller/ComplaintController.java
@@ -0,0 +1,43 @@
+package com.praveen.hms.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.praveen.hms.entity.Complaint;
+import com.praveen.hms.service.ComplaintService;
+
+@RestController
+@RequestMapping("/rest/complaint")
+public class ComplaintController {
+
+	@Autowired
+	private ComplaintService complaintService;	
+//=======================================================================
+		//Save Details	
+
+	@PostMapping
+	public void saveComplaint(@RequestBody Complaint complaint) {
+		complaintService.saveComplaint(complaint);
+	}
+	
+	@PostMapping("/all")
+	public void saveAllComplaint(@RequestBody List<Complaint> complaints) {
+		complaintService.saveAllComplaint(complaints);
+	}
+//=============================================================================
+		//Get Data details	
+	
+	@GetMapping("/byDisease/{name}")
+	public List<String> getPatientByDiseaseName(@PathVariable("name") String diseaseName) {
+		return complaintService.getPatientByDiseaseName(diseaseName);
+	}
+	
+	
+}
