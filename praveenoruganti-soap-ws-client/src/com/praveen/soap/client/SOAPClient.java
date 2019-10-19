5c0a9c2069fec04540e11a9e1982dc96cbb762c1
praveen <praveen@praveen-PC>
2019-10-20 02:46:10 +0530

    updated

--- praveenoruganti-soap-ws-client/src/com/praveen/soap/client/SOAPClient.java
+++ praveenoruganti-soap-ws-client/src/com/praveen/soap/client/SOAPClient.java
@@ -13,8 +13,9 @@ public class SOAPClient {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		String httpRequestURL = "http://localhost:8080/praveenoruganti-apachecxf-jaxws-bottom-up/services/book/BookService?wsdl";
-		String requestXmlParam = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.soap.apachecx.praveen.com/\">"
+		//String httpRequestURL = "http://localhost:8080/praveenoruganti-apachecxf-jaxws-bottom-up/services/book/BookService?wsdl";
+		String httpRequestURL = "http://localhost:8080/praveenoruganti-apachecxf-jaxws-top-down/services/book/BookService?wsdl";
+		String requestXmlParam = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.soap.apachecxf.praveen.com/\">"
 				+ " <soapenv:Header/>" + "<soapenv:Body>" + "<ser:getBookNameBasedOnISBN>" + "<arg0>ISBN-2134</arg0>"
 				+ " </ser:getBookNameBasedOnISBN>" + "</soapenv:Body>" + "</soapenv:Envelope>";
 		System.out.println(httpRequestURL);
