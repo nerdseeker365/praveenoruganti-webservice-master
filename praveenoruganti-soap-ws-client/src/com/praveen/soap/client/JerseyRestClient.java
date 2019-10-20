735b17650916ffa05a9be4548b2bbd6a38b82f7b
praveen <praveen@praveen-PC>
2019-10-20 16:07:01 +0530

    updated

--- praveenoruganti-soap-ws-client/src/com/praveen/soap/client/JerseyRestClient.java
+++ praveenoruganti-soap-ws-client/src/com/praveen/soap/client/JerseyRestClient.java
@@ -8,7 +8,7 @@ public class JerseyRestClient {
 	public static void main(String[] args) {
 		Client client = ClientBuilder.newClient();
 
-		String result = client.target("http://localhost:8080/praveenoruganti-jersey-jaxrs/restapi/myresource").request().get(String.class);
+		String result = client.target("http://localhost:8080/praveenoruganti-jersey-jaxrs-master/restapi/myresource").request().get(String.class);
 
 		System.out.println(result);
 	}
